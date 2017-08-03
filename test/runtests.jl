d43bd62b6f0a2fb946c125ac5886c4a0fc4354f6
Chris de Graaf <chrisadegraaf@gmail.com>
2017-08-03 18:30:28 -0500

    Add basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using Chatter
 using Base.Test
 
-# Write your own tests here.
-@test 1 == 2
+struct Foo <: Chatter.Source end
+
+client = ChatterClient(Foo())
+push!(client.messages, "A", "B", "C", "D", "E", "F", "G")
+
+@test get!(client) == ["A"]
+@test get!(client; n=2) == ["B", "C"]
+@test flush!(client) == ["D", "E", "F", "G"]
+@test isempty(client.messages)
